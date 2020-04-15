b67a0a3f7ab41c6f33692b479c05e7815f284c4f
anshumantsagar <anshumantsagar@gmail.com>
2020-04-15 19:21:22 +0530

    ngx-chips

--- src/app/tag-input/tag-input.component.ts
+++ src/app/tag-input/tag-input.component.ts
@@ -8,24 +8,27 @@ import {Component,OnInit} from '@angular/core'
 
     <tag-input [editable]='true' [maxItems]='5' (onTagEdited)="onTagEdited($event)" [(ngModel)]='items'></tag-input>
     <button (click)="thisFunction()" type="button">Save</button>
-    <div *ngFor="let items of newItems"><h4>{{items}}</h4></div>
+    <div *ngFor="let items of newItems"><h4>{{items.value}}</h4></div>
     
     `,
 })
 
 export class TaginputComponent {
-    items:Array<any>
+    items:Array<any>=[];
     newItems:Array<any>=[];
     constructor(){}
     onTagEdited(event:any){
         console.log('chala')
         console.log(event.value)
-        this.newItems = this.items
+        this.newItems = this.items.value
+        console.log(this.items)
+        console.log(this.newItems)
     }
     thisFunction(){
         console.log("button clicked")
         console.log(this.items)
-        this.newItems.push(this.items)
+        this.newItems = this.items
+        console.log(this.newItems)
 
     }
     
